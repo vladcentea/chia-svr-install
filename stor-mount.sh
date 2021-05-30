ac2b3240facd1ddf2a05516bacd83b1eb8834512
Vlad Centea <60666688+vladcentea@users.noreply.github.com>
2021-05-30 21:29:02 +0200

    Create stor-mount.sh

---
+++ stor-mount.sh
@@ -0,0 +1,28 @@
+mkdir /mnt/stor1-12x8
+mkdir /mnt/stor2-12x8
+mkdir /mnt/stor3-12x8
+mkdir /mnt/stor4-12x8
+mkdir /mnt/stor5-12x8
+mkdir /mnt/stor6-12x8
+mkdir /mnt/stor7-12x8
+mkdir /mnt/stor8-12x8
+mkdir /mnt/stor9-12x8
+mkdir /mnt/stor10-12x8
+mkdir /mnt/stor11-12x8
+mkdir /mnt/stor12-12x8
+
+sudo mount 10.32.51.170:/mnt -t nfs /mnt/stor1-12x8
+sudo mount 10.32.51.169:/mnt -t nfs /mnt/stor2-12x8
+sudo mount 10.32.51.171:/mnt -t nfs /mnt/stor3-12x8
+sudo mount 10.32.51.180:/mnt -t nfs /mnt/stor4-12x8
+sudo mount 10.32.51.173:/mnt -t nfs /mnt/stor5-12x8
+sudo mount 10.32.51.175:/mnt -t nfs /mnt/stor6-12x8
+sudo mount 10.32.51.181:/mnt -t nfs /mnt/stor7-12x8
+sudo mount 10.32.51.176:/mnt -t nfs /mnt/stor8-12x8
+sudo mount 10.32.51.177:/mnt -t nfs /mnt/stor9-12x8
+sudo mount 10.32.51.179:/mnt -t nfs /mnt/stor10-12x8
+sudo mount 10.32.51.174:/mnt -t nfs /mnt/stor11-12x8
+sudo mount 10.32.51.178:/mnt -t nfs /mnt/stor12-12x8
+
+
+
