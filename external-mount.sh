8a78f47220ba6e97f29771f76f21d3a91647291c
Vlad Centea <60666688+vladcentea@users.noreply.github.com>
2021-05-31 15:47:46 +0200

    Create external-mount.sh

---
+++ external-mount.sh
@@ -0,0 +1,13 @@
+sudo mount 5.79.71.53:/mnt -t nfs /mnt/stor1-12x8
+sudo mount 95.211.134.229:/mnt -t nfs /mnt/stor2-12x8
+sudo mount 212.32.236.219:/mnt -t nfs /mnt/stor3-12x8
+sudo mount 37.48.102.76:/mnt -t nfs /mnt/stor4-12x8
+sudo mount 37.48.102.79:/mnt -t nfs /mnt/stor5-12x8
+sudo mount 85.17.71.195:/mnt -t nfs /mnt/stor6-12x8
+sudo mount 85.17.182.245:/mnt -t nfs /mnt/stor7-12x8
+sudo mount 85.17.218.3:/mnt -t nfs /mnt/stor8-12x8
+sudo mount 85.17.218.35:/mnt -t nfs /mnt/stor9-12x8
+sudo mount 95.211.196.97:/mnt -t nfs /mnt/stor10-12x8
+sudo mount 85.17.131.8:/mnt -t nfs /mnt/stor11-12x8
+sudo mount 85.17.73.250:/mnt -t nfs /mnt/stor12-12x8
+
