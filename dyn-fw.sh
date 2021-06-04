03bbb8feb627480e41d0e52628ad3d3caceef7af
Vlad Centea <60666688+vladcentea@users.noreply.github.com>
2021-06-04 19:59:56 +0200

    Update dyn-fw.sh

--- dyn-fw.sh
+++ dyn-fw.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+while :
+do
+
 DYNHOST=next.cloud.exige.lu
 DYNHOST=${DYNHOST:0:28}
 DYNIP=$(host $DYNHOST | grep -iE "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" |cut -f4 -d' '|head -n 1)
@@ -19,3 +22,7 @@ fi
 
 
 sudo ufw allow from $DYNIP to any port 5901
+
+echo renew IP: $DYNIP
+sleep 600
+done
